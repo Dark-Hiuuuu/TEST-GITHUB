7fc663786c20158c2654ae8f3f104ec7caa56ddc
Dark-Hiuuuu <hieuhd2502@gmail.com>
2023-04-28 23:22:13 +0700

    add class properties for cat.py

--- cat.py
+++ cat.py
@@ -1,3 +1,8 @@
 class Cat:
-    pass
+    def __init__(self,name,feature,skin,say):
+        self.name = name
+        self.feature = feature 
+        self.skin = skin 
+        self.say = say
 # sử dụng hướng đối tượng xây con mèo nha Anh Lực
+# xây dựng trên các thuộc tính này nhan anh Lực deadline 5-5 nha anh
