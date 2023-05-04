bcab316dc9707e422192d99ae0cfad14bc215670
CodersTokyo <info@coders.tokyo>
2023-05-04 08:47:41 +0700

    update function for Mouse.py

--- mouse.py
+++ mouse.py
@@ -1,1 +1,11 @@
 # CAO HUỲNH CHI NA XÂY DỰNG LẠI FUNCTION cho mouse trong file này 
+class Mouse:
+    def __init__(self,weight,color,say,type_mouse):
+        self.weight = weight
+        self.color = color
+        self.say  = say 
+        self.type_mouse = type_mouse
+    def sayhi(self):
+        print(self.say)
+    def play(self):
+        pass
